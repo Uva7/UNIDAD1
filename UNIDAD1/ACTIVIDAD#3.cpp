fbc98e5532882f1731d1c1ca5f766c7d2edd1053
Uva7 <167231209+Uva7@users.noreply.github.com>
2024-04-16 11:26:29 -0500

    Add files via upload

---
+++ UNIDAD1/ACTIVIDAD#3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//Mensaje de bienvenida
+	cout<<"Hola! Este programa 1.9 calcula el cuadrado y el cubo de un numero entero positivo"<< "\n";
+	
+	int n;
+	cout<< "Ingresa un numero: ";
+	cin >> n;
+
+	cout<< "El numero elevado al cuadrado es: "<<n*n;
+	
+	cout<< "El numero elevado al cubo es: "<<n*n*n;
+	return 0;
+}
